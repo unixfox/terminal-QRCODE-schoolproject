f13fa596c0920235555bda2b4b9aea32ebc56d2a
Emilien Devos <contact@emiliendevos.be>
2017-06-08 22:23:25 +0200

    Add capture and final script

--- zbarcam.sh
+++ zbarcam.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 while :
 do
-	DATE=$(date)
-	start="08:00:00"
-    end="17:00:00"
 	trap ctrl_c INT
 	endScan=0;
 	function ctrl_c() {
 	 endScan=1;
 	}
 	tmp=/tmp/barcode.$$
-	LD_PRELOAD=/usr/lib/libv4l/v4l1compat.so zbarcam --raw > $tmp &
+	zbarcam --raw > $tmp &
 	pid=$!
 	while [[ ! -s $tmp ]] ; do
 	 sleep 1
@@ -25,7 +22,8 @@ do
 	echo $CODE
 	surname="$(sqlite3 database.db 'select surname FROM "default" where id = '$CODE';')"
 	name="$(sqlite3 database.db 'select name FROM "default" where id = '$CODE';')"
-	zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=7 --title="Vérification de présence" &
-	fswebcam -d /dev/video1 -r 640x480 --jpeg 85 -D 7 capture/webcam-shot-$(date +%s).jpg
+	zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=10 --title="Vérification de présence"
+	zenity --info --text "Capture en cours...\nVeuillez regarder l'objectif." --timeout=3 --title="Vérification de présence"
+	fswebcam -d /dev/video1 -r 640x480 --jpeg 100 -D 3 capture/webcam-shot-$(date +%s).jpg
 	zenity --info --text "Merci ! Photo effectuée, bonne journée !" --timeout=3 --title="Vérification de présence"
 done
