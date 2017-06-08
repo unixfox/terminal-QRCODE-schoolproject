03612a8349a959ab525646febc46a8ebd70a283f
Emilien Devos <contact@emiliendevos.be>
2017-06-08 21:43:26 +0200

    gitignore

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ School project : terminal for managing the presences of the students
 - Linux distribution
 - Install zbar, sqlite3, for example on Debian (and Ubuntu):
 ```
-sudo apt-get install -y zbar sqlite3 zenity
+sudo apt-get install -y zbar sqlite3 zenity fswebcam
 ```
 - Two camera
--- zbarcam.sh
+++ zbarcam.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 while :
 do
+	DATE=$(date)
+	start="08:00:00"
+    end="17:00:00"
 	trap ctrl_c INT
 	endScan=0;
 	function ctrl_c() {
 	 endScan=1;
 	}
 	tmp=/tmp/barcode.$$
-	zbarcam --raw > $tmp &
+	LD_PRELOAD=/usr/lib/libv4l/v4l1compat.so zbarcam --raw > $tmp &
 	pid=$!
 	while [[ ! -s $tmp ]] ; do
 	 sleep 1
@@ -22,5 +25,7 @@ do
 	echo $CODE
 	surname="$(sqlite3 database.db 'select surname FROM "default" where id = '$CODE';')"
 	name="$(sqlite3 database.db 'select name FROM "default" where id = '$CODE';')"
-	zenity --info --text "Bonjour $surname $name !\n Veuillez regardez la caméra Microsoft pour prendre\n une photo de vous attestant que vous êtes bien $surname $name." --title="Vérification d'élève"
+	zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=7 --title="Vérification de présence" &
+	fswebcam -d /dev/video1 -r 640x480 --jpeg 85 -D 7 capture/webcam-shot-$(date +%s).jpg
+	zenity --info --text "Merci ! Photo effectuée, bonne journée !" --timeout=3 --title="Vérification de présence"
 done
