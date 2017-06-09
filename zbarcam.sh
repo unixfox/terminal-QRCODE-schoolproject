51bca1a389d8609b98d1c79e966da4ce3df521e6
Emilien Devos <unixfox@users.noreply.github.com>
2017-06-09 15:25:58 +0200

    Add files via upload

--- zbarcam.sh
+++ zbarcam.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 while :
 do
-	trap ctrl_c INT
+	# ls /dev/video*
+        cameraqr=0
+        cameraphoto=1
+        prescale="320x320"
+        trap ctrl_c INT
 	endScan=0;
 	function ctrl_c() {
 	 endScan=1;
 	}
 	tmp=/tmp/barcode.$$
-	zbarcam --raw > $tmp &
+	zbarcam /dev/video"$cameraqr" --prescale="$prescale" --raw > $tmp &
 	pid=$!
 	while [[ ! -s $tmp ]] ; do
 	 sleep 1
@@ -22,8 +26,8 @@ do
 	echo $CODE
 	surname="$(sqlite3 database.db 'select surname FROM "default" where id = '$CODE';')"
 	name="$(sqlite3 database.db 'select name FROM "default" where id = '$CODE';')"
-	zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=10 --title="Vérification de présence"
-	zenity --info --text "Capture en cours...\nVeuillez regarder l'objectif." --timeout=3 --title="Vérification de présence"
-	fswebcam -d /dev/video1 -r 640x480 --jpeg 100 -D 3 capture/webcam-shot-$(date +%s).jpg
-	zenity --info --text "Merci ! Photo effectuée, bonne journée !" --timeout=3 --title="Vérification de présence"
+	#zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=5 --title="Vérification de présence"
+	zenity --info --text "Capture en cours...\nVeuillez regarder l'objectif." --timeout=1 --title="Vérification de présence" &
+	fswebcam -d /dev/video"$cameraphoto" -r 640x480 --jpeg 100 -D 1 capture/webcam-shot-$(date +%s).jpg
+	zenity --info --text "Merci ! Photo effectuée, bonne journée !" --timeout=2 --title="Vérification de présence"
 done
