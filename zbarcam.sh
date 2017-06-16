8d061966fd6e82f26b7fb72fd94e628189f7f6a5
Emilien Devos <unixfox@users.noreply.github.com>
2017-06-16 20:28:58 +0200

    Update zbarcam.sh

--- zbarcam.sh
+++ zbarcam.sh
@@ -2,17 +2,25 @@
 while :
 do
 	# ls /dev/video*
-        cameraqr=0
-        cameraphoto=1
-        prescale="320x320"
-        trap ctrl_c INT
+    cameraqr=0
+    cameraphoto=1
+    # Dimension de la fenêtre du scan du QR Code
+    prescale="320x320"
+    # Récupérer la date actuelle sous forme de timestamp Unix
+    DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+    # Dès qu'un CTRL + C est pressé ça quitte le programme.
+    trap ctrl_c INT
 	endScan=0;
 	function ctrl_c() {
 	 endScan=1;
 	}
+	# Variable définissant l'endroit où sera stocké le fichier temporaire concenant la valeur inscrit sur le QR Code.
 	tmp=/tmp/barcode.$$
+	# Lancement de Zbarcam
 	zbarcam /dev/video"$cameraqr" --prescale="$prescale" --raw > $tmp &
+	# Récupération du numéro du processus Zbarcam
 	pid=$!
+	# Boucle liée au CTRL + C
 	while [[ ! -s $tmp ]] ; do
 	 sleep 1
 	 if [ $endScan == 1 ]; then
@@ -20,14 +28,20 @@ do
 	 	exit 1
 	 fi
 	done
+	# On tue le processus une fois qu'on a récupéré la valeur inscrit sur le QR Code.
 	kill $pid
+	# Lecture du fichier temporaire et puis sauvegarde de sa valeur dans une variable.
 	CODE=$(cat $tmp)
+	# Suppression du fichier temporaire
 	rm $tmp
-	echo $CODE
+	# Récupération du prénom depuis la base de données
 	surname="$(sqlite3 database.db 'select surname FROM "default" where id = '$CODE';')"
+	# Récupération du nom depuis la base de données
 	name="$(sqlite3 database.db 'select name FROM "default" where id = '$CODE';')"
-	#zenity --info --text "Bonjour $surname $name !\nVeuillez regardez la caméra Microsoft jusqu'à ce que le témoin vert apparait\npour prendre une photo de vous attestant que vous êtes bien $surname $name." --timeout=5 --title="Vérification de présence"
-	zenity --info --text "Capture en cours...\nVeuillez regarder l'objectif." --timeout=1 --title="Vérification de présence" &
-	fswebcam -d /dev/video"$cameraphoto" -r 640x480 --jpeg 100 -D 1 capture/webcam-shot-$(date +%s).jpg
-	zenity --info --text "Merci ! Photo effectuée, bonne journée !" --timeout=2 --title="Vérification de présence"
+	# Affichage d'une boite de dialogue
+	gxmessage -center -geometry 300x50 --wrap "Bonjour $surname $name !" -timeout 2 -title "Vérification de présence" &
+	# Prise d'une photo de la personne via la deuxième caméra.
+	fswebcam -d /dev/video"$cameraphoto" -r 640x480 --jpeg 100 -D 2 capture/"$surname"-"$name"-"$DATE".jpg
+	# Affichage d'une boite de dialogue n°2
+	gxmessage -center -geometry 300x50 --wrap "Bonne journée !" -timeout 2 -title "Présence Vérifiée"
 done
